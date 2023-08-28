a5f0a6cb2c24073deeae5e7d7061ca2d645600de
darrick fabian <darrick@darrick.org>
2023-08-27 21:07:01 -0700

    Update yaml install x5

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,28 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Google', async ({ page }) => {
+  // Navigate to the Google homepage
+  await page.goto('https://www.google.com');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Type a search query in the search input field
+  await page.fill('textarea[name="q"]', 'Playwright testing');
+
+  // Press the "Enter" key to perform the search
+  await page.press('textarea[name="q"]', 'Enter');
+
+  // Wait for the search results to load
+  await page.waitForSelector('#search');
+
+  // Get the title of the search results page
+  const pageTitle = await page.title();
+
+  // Verify that the title contains the search query
+  if (pageTitle.includes('Playwright testing')) {
+    console.log('Test passed: Search results page title is correct');
+  } else {
+    console.error('Test failed: Search results page title is incorrect');
+  }
 });
 
 test('get started link', async ({ page }) => {
